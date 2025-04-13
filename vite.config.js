02f8bccc15923c872cf7e00ed4e97030f2566f9a
martinbrumana <martinbrumana@gmail.com>
2025-04-13 11:21:45 -0300

    nw vite

--- vite.config.js
+++ vite.config.js
@@ -8,13 +8,38 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: "autoUpdate",
-      includeAssets: ["favicon.svg", "robots.txt"],
-      manifest: false,
-      devOptions: {
-        enabled: true,
+      includeAssets: [
+        "favicon.svg",
+        "robots.txt",
+        "pwa-192x192.png",
+        "pwa-512x512.png",
+      ],
+      manifest: {
+        name: "usTasks",
+        short_name: "usTasks",
+        description: "Tu organizador personal y compartido",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#ffffff",
+        theme_color: "#0d6efd",
+        icons: [
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
       },
       workbox: {
-        globPatterns: ["**/*.{js,css,html,ico,png,svg}"], // ✅ importante
+        globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+      },
+      devOptions: {
+        enabled: true,
       },
     }),
   ],
